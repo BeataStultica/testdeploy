0d88aca54caf758d400c826a81a276414dccf9cb
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 21:11:03 +0300

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.get("/", function (request, response) {
   response.send("Hello World!");
 });
 
-var port = 5000;
+console.log(process.env.PORT);
+var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log("Listening on " + port);
 });
